6dc567a993024145216155c5f03329ab1c208bb3
jackszb <55304530+jackszb@users.noreply.github.com>
2026-07-17 22:25:53 +0800

    Update rule sources to use iphone-simple-rules

--- scripts/update_rules.py
+++ scripts/update_rules.py
@@ -24,17 +24,17 @@ RULES = [
         "geoip-cn.srs",
     ),
     (
-        "https://raw.githubusercontent.com/jackszb/merge-rules/main/rules/direct.json",
+        "https://raw.githubusercontent.com/jackszb/iphone-simple-rules/main/rules/direct.json",
         "direct.json",
         "geosite-cn.srs",
     ),
     (
-        "https://raw.githubusercontent.com/jackszb/merge-rules/main/rules/proxy.json",
+        "https://raw.githubusercontent.com/jackszb/iphone-simple-rules/main/rules/proxy.json",
         "proxy.json",
         "geosite-!cn.srs",
     ),
     (
-        "https://raw.githubusercontent.com/jackszb/merge-rules/main/rules/reject.json",
+        "https://raw.githubusercontent.com/jackszb/iphone-simple-rules/main/rules/reject.json",
         "reject.json",
         "adblock.srs",
     ),
